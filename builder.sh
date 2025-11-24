016f40d655e0af3d4f0f61a8671dadf3388ddb61
Rage Fade <ragef6@tutanota.de>
2025-11-24 13:19:45 -0500

    Made all binaries Executables on UNIX

--- builder.sh
+++ builder.sh
@@ -3,5 +3,6 @@ mkdir -p execs
 for arch in amd64 arm64; do
   for os in windows darwin linux; do
     GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o=execs/"3xp1-$os-$arch"
+    chmod +x execs/*
   done
 done
