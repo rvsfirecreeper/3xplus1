c3d1a381c91e60de7167853ffd55d8d4ac0029c4
Rage Fade <ragef6@tutanota.de>
2025-11-28 16:30:40 -0500

    Deleted builder.sh

--- builder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p execs
-for arch in amd64 arm64; do
-  for os in windows darwin linux; do
-    GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o=execs/"3xp1-$os-$arch"
-    chmod +x execs/*
-  done
-done
